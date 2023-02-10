e28c3cf198b1f334c8ea7e9f2ca5bf3ee966e7b9
Chubkey <69558928+Ravager6969@users.noreply.github.com>
2023-02-09 19:17:50 -0800

    started websocket implementation

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,88 @@
 const express = require('express')
 const crypto = require('crypto')
-const socket = require('socket.io')
-const server = require('http').createServer();
+const {Server} = require('socket.io')
+
+const data = require('./data.json')
 
 const app = express();
-const io = socket(server);
+
+const server = require('http').createServer(app);
+
+const players = {
+    "1": "Jack Hoff",
+    "2": "Ben Dover"
+}
+
+const rooms = {
+    "abc": {users: [], guessed: 0}
+}
+
+const getRandomLocation = () => {
+    return data[Math.floor(Math.random()*data.length)]
+}
+/*
+started: boolean,
+users: guessed: boolean
+
+*/
+
+const io = new Server(server);
 /*
 socket stuff
 */
-io.on('connection', function(socket){
-    socket.emit('hi')
+io.on("connection", (socket) => {
+    
+    socket.on("join", (r) => {
+        const req = JSON.parse(r)
+
+        if (!(req.room in rooms)){
+            socket.emit('room_not_found', `No Room Exists with the ID: ${req.room}`)   
+        } else {
+            socket.join(req.room)
+            
+            if (!(rooms[req.room].users.includes(req.user))){
+                rooms[req.room].users.push(req.user)
+                io.to(req.room).emit('room', rooms[req.room].users)
+            }   
+        }
+    })
+
+    socket.on("start", (room) => {
+        if (rooms.includes(room)){
+            //rooms[room].started = true;
+            io.to(room).emit('start')
+            io.to(room).emit('location', getRandomLocation())
+        } else {
+            socket.emit('room_not_found', `No Room Exists with the ID: ${room}`)
+        }
+    })
+
+    socket.on("guess", (data) => { 
+        //guess is LatLng object
+        const guessData = JSON.parse(data)
+        //manage countdown
+        console.log(guessData)
+        io.to(guessData.room).emit('guess', guessData.guess)
+
+        rooms[guessData.room].guessed = rooms[guessData.room].guessed
+
+        if (rooms.guessData.room.guessed === rooms[guessData.room].users.length){
+            io.to(guessData.room).emit('round_over')
+        }
+        
+    })
+
+    socket.on("new_room", () => {
+        
+    })
+    //socket.emit('message', 'hi')
+    console.log(socket.id)
+    io.emit()
+
+    socket.on('message', (msg) => {
+        socket.emit('msg', msg)
+    })
+    //io.to('room1').emit('you are in room 1')
 })
 
 
@@ -26,6 +99,6 @@ app.listen(3001, () => {
     console.log('Server running on port 3001')
 })
 
-server.listen(3002, function(){
+server.listen(3002, () => {
     console.log('Socket running on port 3002')
 })
---
+++ frontend/.next/server/pages/api/hello.js
@@ -0,0 +1,36 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/api/hello";
+exports.ids = ["pages/api/hello"];
+exports.modules = {
+
+/***/ "(api)/./src/pages/api/hello.ts":
+/*!********************************!*\
+  !*** ./src/pages/api/hello.ts ***!
+  \********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ handler)\n/* harmony export */ });\n// Next.js API route support: https://nextjs.org/docs/api-routes/introduction\nfunction handler(req, res) {\n    res.status(200).json({\n        name: \"John Doe\"\n    });\n}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwaSkvLi9zcmMvcGFnZXMvYXBpL2hlbGxvLnRzLmpzIiwibWFwcGluZ3MiOiI7Ozs7QUFBQSw2RUFBNkU7QUFPOUQsU0FBU0EsUUFDdEJDLEdBQW1CLEVBQ25CQyxHQUEwQixFQUMxQjtJQUNBQSxJQUFJQyxNQUFNLENBQUMsS0FBS0MsSUFBSSxDQUFDO1FBQUVDLE1BQU07SUFBVztBQUMxQyxDQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vZ2VvZ3Vlc3ItbmV4dGpzLy4vc3JjL3BhZ2VzL2FwaS9oZWxsby50cz9iMmE3Il0sInNvdXJjZXNDb250ZW50IjpbIi8vIE5leHQuanMgQVBJIHJvdXRlIHN1cHBvcnQ6IGh0dHBzOi8vbmV4dGpzLm9yZy9kb2NzL2FwaS1yb3V0ZXMvaW50cm9kdWN0aW9uXG5pbXBvcnQgdHlwZSB7IE5leHRBcGlSZXF1ZXN0LCBOZXh0QXBpUmVzcG9uc2UgfSBmcm9tICduZXh0J1xuXG50eXBlIERhdGEgPSB7XG4gIG5hbWU6IHN0cmluZ1xufVxuXG5leHBvcnQgZGVmYXVsdCBmdW5jdGlvbiBoYW5kbGVyKFxuICByZXE6IE5leHRBcGlSZXF1ZXN0LFxuICByZXM6IE5leHRBcGlSZXNwb25zZTxEYXRhPlxuKSB7XG4gIHJlcy5zdGF0dXMoMjAwKS5qc29uKHsgbmFtZTogJ0pvaG4gRG9lJyB9KVxufVxuIl0sIm5hbWVzIjpbImhhbmRsZXIiLCJyZXEiLCJyZXMiLCJzdGF0dXMiLCJqc29uIiwibmFtZSJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(api)/./src/pages/api/hello.ts\n");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-api-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("(api)/./src/pages/api/hello.ts"));
+module.exports = __webpack_exports__;
+
+})();
