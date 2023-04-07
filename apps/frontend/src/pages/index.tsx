546af87b211a4591664e8cca8a69c9d66b36581b
Jason Louie <jason.jw.louie@gmail.com>
2023-04-07 14:51:18 -0700

    removed node_modules

--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -1,7 +1,10 @@
 import Head from "next/head";
+import { useLayoutEffect } from "react";
 
 export default function Home() {
-
+  useLayoutEffect(() => {
+    window.location.href = "/room/abc"
+  }, [])
   return (
     <>
       <Head>
